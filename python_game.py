f6c21d6f3e6bd3423392ce57960e223f96a71a94
Mintae Kim <mkim221@myseneca.ca>
2020-08-27 15:02:49 -0400

    set the background

--- python_game.py
+++ python_game.py
@@ -8,7 +8,10 @@ screen_height = 640 #height size
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #Screen title
-pygame.display.set_caption("Mintae Game") #game name
+pygame.display.set_caption("Mintae Game") #game name 
+
+#background load
+background = pygame.image.load("/Users/mintae/Desktop/Python_game/pygame_basic/background.png")
 
 #Even loop
 running = True #Check that game is playing
@@ -17,6 +20,11 @@ while running:
     for event in pygame.event.get(): #which event is happend?
         if event.type == pygame.QUIT: #Quit event is happend?
             running = False
+
+    screen.blit(background, (0, 0)) #background locate
+
+    pygame.display.update() #Recall background
+
 
 #Game close
 pygame.quit()
